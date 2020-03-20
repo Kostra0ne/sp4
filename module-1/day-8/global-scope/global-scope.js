373cb3651433323294188deeca8aa6880318b743
KostraOne <guillaume.amangoua@gmail.com>
2020-03-20 17:45:09 +0100

    ajout snippets jour 8 (vendredi) scopes + matrices + nested loops

--- module-1/day-8/global-scope/global-scope.js
+++ module-1/day-8/global-scope/global-scope.js
@@ -1,8 +1,7 @@
-var maVarGlobale = "Je suis gloable, je suis la mal !!!";
+var maVarGlobale = "Je suis une var globale, je suis (souvent) le mal !!! >_<' ";
 
 function test() {
-  var maVarLocal = "je suis locale à la fonction test, j'oeuvre pour le bien";
+  var maVarLocal = "je suis locale à la fonction test, j'oeuvre toujours pour le bien :D";
   console.log(maVarLocal);
-  for (i = 0; i < 100; i++) {}
 }
 
