aeb2093a5e5d937d66a85d41ed892634bc333b81
KostraOne <guillaume.amangoua@gmail.com>
2020-03-23 09:26:29 +0100

    suppression pour réorganisation des dossiers en semaines

--- module-1/day-8/global-scope/global-scope.js
+++
@@ -1,7 +0,0 @@
-var maVarGlobale = "Je suis une var globale, je suis (souvent) le mal !!! >_<' ";
-
-function test() {
-  var maVarLocal = "je suis locale à la fonction test, j'oeuvre toujours pour le bien :D";
-  console.log(maVarLocal);
-}
-
