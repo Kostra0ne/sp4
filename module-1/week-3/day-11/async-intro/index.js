1f1ba8f4db65acae9314b24b650560c51500f241
KostraOne <guillaume.amangoua@gmail.com>
2020-03-25 17:37:46 +0100

    ajout commentaire

--- module-1/week-3/day-11/async-intro/index.js
+++ module-1/week-3/day-11/async-intro/index.js
@@ -13,11 +13,12 @@ const timeOut1 = setTimeout(function() {
   clearTimeout(timeOut1); // clearTimeout permet de supprimer le timeOut de la mémoire...
 }, 3000); // le callback sera exécuté dans 1 seconde
 
-
-const timeOut2 = setTimeout(() => {
+const callback = () => {
   console.log("Yata: action 2 finished !!!");
   clearTimeout(timeOut2); // clearTimeout permet de supprimer le timeOut de la mémoire...
-}, 1000);
+};
+
+const timeOut2 = setTimeout(callback, 1000);
 
 // ATTENTION : donc aux actions acynchrones car elle ne sont pas forcément exécutée dans l'ordre d'éxecution
 
@@ -32,5 +33,4 @@ const intervalID = setInterval(() => {
   console.log("hey JS c'est fun (même si un peu dur)", count);
 
   if (count === 10) clearInterval(intervalID);
-
 }, 1000); // le callback fournit en argument d'un setInterval sera exécuté à interval régulier
