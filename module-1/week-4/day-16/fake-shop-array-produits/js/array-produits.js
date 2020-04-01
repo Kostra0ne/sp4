7708a05337a1f1473bc012bc34d28567dc273bf9
KostraOne <guillaume.amangoua@gmail.com>
2020-04-01 12:16:32 +0200

    modifi consigne array products

--- module-1/week-4/day-16/fake-shop-array-produits/js/array-produits.js
+++ module-1/week-4/day-16/fake-shop-array-produits/js/array-produits.js
@@ -1,4 +1,4 @@
 // Utiliser le DOM pour réaliser la tâche suivante
 // -- préparer une liste (array) de produits (objects) dans votre programme JS
-// -- afficher les produits depuis une fonction
+// -- afficher les produits dans le HTML depuis une fonction JS
 // -- bonus: chaque produit est une instance de classe Product
