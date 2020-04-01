0bb6f67f1d12d826ab4a33366fcf90b1e6757421
KostraOne <guillaume.amangoua@gmail.com>
2020-04-01 14:20:40 +0200

    mise à jour soluce js-dom-session-1

--- module-1/_TPS/js-dom-sessions/soluce-session-1/js-dom-session-1.js
+++ module-1/_TPS/js-dom-sessions/soluce-session-1/js-dom-session-1.js
@@ -1,8 +1,12 @@
 // DECLARATION DES FONCTIONS DES ITERATIONS
 
+// itération 1
+
 function displayText(msg) {
   alert(msg);
 }
+
+// itération 2
 
 function multiplier(a, b) {
   let res = a * b;
@@ -23,7 +27,93 @@ function afficherResultDansElement(idElement, res) {
   if (target) target.innerHTML = res;
 }
 
+// itération 3
+const chanteurs = [
+  {
+    nom: "Beyoncé",
+    email: "formation@diva.com",
+    age: 40
+  },
+  {
+    nom: "Booba",
+    email: "easy@zer.com",
+    age: 40
+  },
+  {
+    nom: "Rohff",
+    email: "shopping@zonz.fr",
+    age: 40
+  },
+  {
+    nom: "Kaaris",
+    email: "loose@win.com",
+    age: 40
+  }
+];
+
+function parcourirChanteurs(list) {
+  list.forEach(chanteur => {
+    afficherChanteur(chanteur);
+  });
+}
+
+function afficherChanteur(chanteur) {
+  document.getElementById(
+    "chanteurs"
+  ).innerHTML += `<div class="chanteur">l'artiste ${chanteur.nom}, 
+      âgé de ${chanteur.age} est joignable à l'adresse ${chanteur.email}
+    </div>`;
+}
+
+function parcourirChanteurs2(list) {
+  list.forEach(chanteur => {
+    document.getElementById(
+      "chanteurs"
+    ).innerHTML += `<div class="chanteur">l'artiste ${chanteur.nom}, 
+      âgé de ${chanteur.age} est joignable à l'adresse ${chanteur.email}
+    </div>`;
+  });
+}
+
+// itération 4/5
+
+function parcourirBlock(blocks) {
+  blocks.forEach(block => {
+    // block.addEventListener("click", styliserBlock);
+    // block.onclick = styliserBlock; // synonyme
+    block.onclick = styliserBlockAvecCSS; // synonyme
+    afficherIdBlock(block.id);
+  });
+}
+
+function styliserBlockAvecCSS(evt) {
+  evt.target.classList.toggle("is-active");
+}
+
+function styliserBlock(evt) {
+  // console.log(evt.target); // correspond au block clické ...
+  if (evt.target.style.background !== "dodgerblue") {
+    evt.target.style.background = "dodgerblue";
+    evt.target.style.color = "whitesmoke";
+    evt.target.style.borderColor = "black";
+  } else {
+    evt.target.style.background = "none";
+    evt.target.style.color = "black";
+  }
+}
+
+function afficherIdBlock(id) {
+  const resultat = document.getElementById("res-iteration-4");
+  // resultat.textContent += `${id} \n` // @todo : trouver soluce
+  resultat.innerHTML += `id du bock : ${id}<br />`;
+}
+
 // APPEL DES FONCTIONS DES ITERATIONS
+
+const selectionBlocks = document.querySelectorAll("#iteration-4 .block");
+parcourirBlock(selectionBlocks);
+
+parcourirChanteurs2(chanteurs);
 
 // displayText("hello, Acrrochez-vous !");
 
