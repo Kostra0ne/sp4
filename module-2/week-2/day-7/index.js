18e2072ad109287f2d394cbab0026247cf3bc776
KostraOne <guillaume.amangoua@gmail.com>
2020-05-14 17:54:05 +0200

    ajout de l'intro aux exports avec node

--- module-2/week-2/day-7/index.js
+++ module-2/week-2/day-7/index.js
@@ -2,8 +2,10 @@
 require("dotenv").config();
 // la ligne ci-dessus va lire les clé/valeurs du fichier de configuration .env et les mettre à dispo dans tous les fichers de notre app back => // process.env.NOMCLE
 
-// BASE VARS
+const importedValues = require("./test");
+console.log(importedValues.maFunctionDeTest(true))
 
+// BASE VARS
 const express = require("express");
 const hbs = require("hbs");
 const server = express();
@@ -11,17 +13,21 @@ const PORT = process.env.PORT;
 
 // CONFIG STATIC
 server.use(express.static(__dirname + "/static"));
-// CONFIG POST DATA
+// CONFIG récupérer les données postées depuis nos forms 
+// sous la clé req.body (req.body.email, req.body.message, req.body.password)
 server.use(express.urlencoded({ extended: false }));
 // CONFIG TEMPLATING
 server.set("views", __dirname + "/views");
+// view engine : moteur de template
+// on choisit la librairie qui va générer les template de notre app
+// soiot jade, pug, ejs ou dans ici hbs
 server.set("view engine", "hbs");
 
 // ROUTING
 server.get(["/", "/home"], (req, res) => {
   const data = {
     title: "Bienvenue",
-    numbers: [100, 2000, 3, 44],
+    numbers: [100, 2000, 3, 44, importedValues.universalTruth],
     strings: ["yo", "bien", "tout", "???"],
     isOpen: true,
     cat: {
@@ -47,6 +53,36 @@ server.get("/about", (req, res) => {
     ]
   };
   res.render("about", data);
+});
+
+server.get("/contact", (req, res) => {
+  const data = {
+    title: "Nous contacter",
+    subjects: [
+      {
+        value: "information",
+        text: "Information générales"
+      },
+      {
+        value: "recrutement",
+        text: "Postuler"
+      },
+      {
+        value: "jeux-concours",
+        text: "Jeux concours"
+      },
+      {
+        value: "SAV",
+        text: "Service après-vente"
+      },
+    ]
+  };
+  res.render("contact", data);
+});
+
+server.post("/contact", (req, res) => {
+  console.log(req.body); // les données postées depuis le formulaire de contact
+  res.send("done");
 });
 
 server.get("/contact", (req, res) => {
---
+++ module-2/week-2/day-7/intro-export-node.js
@@ -0,0 +1,10 @@
+const universalTruth = 42;
+
+const maFunctionDeTest = function (x) {
+  return typeof x;
+};
+
+module.exports = {
+  universalTruth,
+  maFunctionDeTest,
+};
