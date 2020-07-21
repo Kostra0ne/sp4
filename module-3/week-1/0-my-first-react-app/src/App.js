4e253fb1ef8a3acf7c61c272c4e2ac75333f0a59
KostraOne <guillaume.amangoua@gmail.com>
2020-07-21 13:58:26 +0200

    added users card feature

--- module-3/week-1/0-my-first-react-app/src/App.js
+++ module-3/week-1/0-my-first-react-app/src/App.js
@@ -1,22 +1,87 @@
 import React from "react";
 
 import { FirstOne } from "./components/baby-steps/FirstOne";
+import { UserCard } from "./components/practice/UserCard";
 import MyButton from "./components/baby-steps/MyButton";
 import MyNumber from "./components/baby-steps/MyNumber";
 import DestructurerProps from "./components/baby-steps/DestructurerProps";
 
 function App() {
-  const action1 = () => console.log("action 1");
+  const pageTitle = "Welcome @ React";
+  const users = [
+    {
+      firstName: "Jean",
+      lastName: "Sanchez",
+      email: "foo@bar.baz",
+      gender: "f",
+      age: 63,
+      picture: "https://randomuser.me/api/portraits/women/13.jpg",
+    },
+    {
+      firstName: "David",
+      lastName: "Banner",
+      email: "foo@bar.baz",
+      gender: "m",
+      age: 43,
+      picture: "https://randomuser.me/api/portraits/men/13.jpg",
+    },
+    {
+      firstName: "Donna",
+      lastName: "Summers",
+      email: "foo@bar.baz",
+      gender: "f",
+      age: 33,
+      picture: "https://randomuser.me/api/portraits/women/23.jpg",
+    },
+    {
+      firstName: "Bill",
+      lastName: "Baroudeur",
+      email: "foo@bar.baz",
+      gender: "m",
+      age: 23,
+      picture: "https://randomuser.me/api/portraits/men/23.jpg",
+    },
+  ];
+
+  /**
+   * IMPORTANT : la function suivante ne se comporte pas comme on pourrait le croire
+   * Pour modifier l'état (state) d'un component react, il faudra utiliser d'autres stratégies ;)
+   */
+  const pushUser = () => {
+    console.log("action 1 : push user");
+    users.push({
+      firstName: "Kim",
+      lastName: "NewChallenger",
+      email: "kim@playa.baz",
+      gender: "?",
+      age: 42,
+    });
+    console.log(">> updated users ??? ", users);
+  };
+
   const action2 = () => console.log("action 2");
   const action3 = () => console.log("action 3");
 
   return (
-    // expression JSX
+    // retourne une expression JSX
     <div className="App">
-      <h1>home</h1>
+      <h1>{pageTitle}</h1>
+
+      <hr />
+      {/* 
+      loop sur chaque user et affiche un component UserCard par user 
+      */}
+      {users.map((u, i) => (
+        <UserCard key={i} user={u} />
+      ))}
+
+      <hr />
+
       <FirstOne />
       <FirstOne />
       <FirstOne />
+
+      <hr />
 
       <p>
         In React : <b>Props goes down</b>{" "}
@@ -25,13 +90,25 @@ function App() {
 
       <hr />
 
-      <DestructurerProps bg="black" loveJS={true} user={{name: "gui", age: 39.99}} />
-      <DestructurerProps bg="gray" loveJS={false} user={{name: "bill", age: 29}} />
-      <DestructurerProps loveJS={false} user={{name: "bill", age: 29}} />
-      <DestructurerProps bg="blue" loveJS={false} user={{name: "bill", age: 29}} />
+      <DestructurerProps
+        bg="black"
+        loveJS={true}
+        user={{ name: "gui", age: 39.99 }}
+      />
+      <DestructurerProps
+        bg="gray"
+        loveJS={false}
+        user={{ name: "bill", age: 29 }}
+      />
+      <DestructurerProps loveJS={false} user={{ name: "bill", age: 29 }} />
+      <DestructurerProps
+        bg="blue"
+        loveJS={false}
+        user={{ name: "bill", age: 29 }}
+      />
 
       <hr />
-      
+
       <MyNumber number="1" />
 
       <MyNumber number={1 + 1} min={0} max={100} />
@@ -40,7 +117,7 @@ function App() {
 
       <hr />
 
-      <MyButton text="create user" callback={action1} />
+      <MyButton text="create user" callback={pushUser} />
       <MyButton text="signin" callback={action2} />
       <MyButton text="signup" callback={action3} />
     </div>
