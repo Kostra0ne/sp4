9e1ee5994a95d1d1eb815fd98a0cfc360a9ca604
KostraOne <guillaume.amangoua@gmail.com>
2020-07-23 21:45:23 +0200

    added product routr

--- module-3/api-server/app.js
+++ module-3/api-server/app.js
@@ -32,6 +32,9 @@ app.use(
   })
 );
 
+app.get("/", (req, res) => res.send("hello :) my api is working"))
+
+app.use("/api/products", require("./routes/api.products"));
 app.use("/api/heroes", require("./routes/api.heroes"));
 
 module.exports = app;
