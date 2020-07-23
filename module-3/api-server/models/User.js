92c81e46f4448bed0226990f9ee8cea7bdb83e6c
KostraOne <guillaume.amangoua@gmail.com>
2020-07-23 21:48:31 +0200

    modified model user

--- module-3/api-server/models/User.js
+++ module-3/api-server/models/User.js
@@ -11,6 +11,6 @@ const userSchema = new Schema({
   },
 });
 
-const UserModel = mongoose.model("Users", userSchema);
+const UserModel = mongoose.model("User", userSchema);
 
 module.exports = UserModel;
