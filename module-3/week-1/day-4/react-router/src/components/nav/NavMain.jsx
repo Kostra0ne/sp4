67ffd88531301b815a6dbd98b957e1db2a2e29cc
KostraOne <guillaume.amangoua@gmail.com>
2020-07-23 21:49:44 +0200

    update mineurs

--- module-3/week-1/day-4/react-router/src/components/nav/NavMain.jsx
+++ module-3/week-1/day-4/react-router/src/components/nav/NavMain.jsx
@@ -4,8 +4,9 @@ import { NavLink } from "react-router-dom";
 export default function NavMain() {
   return (
     <nav id="nav-main">
-      <NavLink exact to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
+      <NavLink exact to="/">Home |</NavLink>
+      <NavLink to="/about">About |</NavLink>
+      <NavLink to="/contact">contact |</NavLink>
     </nav>
   );
 }
