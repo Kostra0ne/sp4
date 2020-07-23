844e94d1145fec999d803dd761d8da85d59495ef
KostraOne <guillaume.amangoua@gmail.com>
2020-07-23 22:29:48 +0200

    added react practice-3.2

---
+++ module-3/week-1/day-5/diy-react-practice-3/src/Likes.jsx
@@ -0,0 +1,13 @@
+// REACT FONTAWESOME !!!!
+// npm i --save @fortawesome/fontawesome-svg-core \
+//              @fortawesome/free-solid-svg-icons \
+//              @fortawesome/react-fontawesome \
+//              @fortawesome/free-regular-svg-icons
+
+// READ THE DOC
+// https://github.com/FortAwesome/react-fontawesome
+
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as FaStarEmpty } from "@fortawesome/free-regular-svg-icons";
