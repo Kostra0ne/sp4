93ab6948463c0e2957409e38aa776722902d2fea
KostraOne <guillaume.amangoua@gmail.com>
2020-07-23 21:48:14 +0200

    fixed bug drop collection

--- module-3/api-server/bin/users.seed.js
+++ module-3/api-server/bin/users.seed.js
@@ -3,11 +3,25 @@ require("./../config/mongo");
 const users = require("./../json/users-fake-db.json").users;
 const UserModel = require("./../models/User");
 
-(async function() {
+(async function () {
   try {
-    await UserModel.collection.drop();
-    await UserModel.create(users);
-  } catch (err) {
-    console.error(err);
+    try {
+      await UserModel.collection.drop();
+      const res = await UserModel.create(users);
+      console.log(res.length + " users created in database");
+    } catch (err) {
+      if (err.code === 26) {
+        console.log(
+          "namespace %s not found, creating collection ",
+          UserModel.collection.name
+        );
+        const res = await UserModel.create(users);
+        console.log(res.length + " users created in database");
+      } else {
+        throw e;
+      }
+    }
+  } catch (e) {
+    console.error(e);
   }
-}())
+})();
