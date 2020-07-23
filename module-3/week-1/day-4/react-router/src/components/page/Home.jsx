aec1f4cdb9e95592be2e6410f484715c0b2dea13
KostraOne <guillaume.amangoua@gmail.com>
2020-07-23 21:47:13 +0200

    added demo page

---
+++ module-3/week-1/day-4/react-router/src/components/page/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <div className="page">
+      <h1 className="title">Home</h1>
+      <h1 className="title">Installation</h1>
+      <p>$ npm i react-router-dom</p>
+      <Link to="/contact">CONTACT GOOOO ...</Link>
+      <Link to="/whatever">fake link here ...</Link>
+      <h2 className="title">API reference</h2>
+      check the doc{" "}
+      <a
+        href="https://reacttraining.com/react-router/web/guides/quick-start"
+        target="_blank"
+      >here</a>
+    </div>
+  );
+}
