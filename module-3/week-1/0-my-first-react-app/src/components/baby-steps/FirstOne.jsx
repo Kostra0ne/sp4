e97b310cfb4d1a8b6b63bfe2a42cd570792b55f0
KostraOne <guillaume.amangoua@gmail.com>
2020-07-23 10:00:48 +0200

    moved my-first-react-app folder in day 1

--- module-3/week-1/0-my-first-react-app/src/components/baby-steps/FirstOne.jsx
+++
@@ -1,8 +0,0 @@
-// rfc + tab =>
-import React from "react";
-
-import "./../../styles/FirstOne.css";
-
-export function FirstOne() {
-  return <div className="first-one">FOO</div>;
-}
--- module-3/week-1/0-my-first-react-app/src/components/baby-steps/MyNumber.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-
-export default function MyNumber(props) {
-  console.log(typeof props, props);
-  return <p>{props.number}</p>;
-}
