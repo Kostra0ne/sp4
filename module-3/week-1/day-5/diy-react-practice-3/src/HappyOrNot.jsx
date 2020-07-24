643a6f9c7ddb8d1159a277a3cc5581083eedb23b
KostraOne <guillaume.amangoua@gmail.com>
2020-07-24 09:26:17 +0200

    added HappyOrNot

---
+++ module-3/week-1/day-5/diy-react-practice-3/src/HappyOrNot.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+const URLLoose = "https://giphy.com/embed/2gWBLrvj8qoYeT3vLS";
+const URLWin = "https://giphy.com/embed/jTlKGJBGQc4FwFEmgj";
+
+export default class HappyOrNot extends Component {
+  state = {
+  };
+
+  render() {
+    return (
+      <div>
+       
+      </div>
+    );
+  }
+}
