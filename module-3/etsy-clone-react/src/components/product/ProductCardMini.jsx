15ebc1c3eb2906129b1c05c83ed9e6c5a8f76daa
KostraOne <guillaume.amangoua@gmail.com>
2020-07-28 09:41:12 +0200

    fixed typo

--- module-3/etsy-clone-react/src/components/product/ProductCardMini.jsx
+++ module-3/etsy-clone-react/src/components/product/ProductCardMini.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export default function ProductCardMini({ infos }) {
-  console.log(infos);
+  // console.log(infos);
   return (
-    <li class="product card mini">
+    <li className="product card mini">
       <img className="img" src={infos.image} alt="produc foo" />
       <span className="price">{infos.price} euros</span>
     </li>
