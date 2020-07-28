337f2b79fcc4bbbc39db0fdb4b22856c0a51d743
KostraOne <guillaume.amangoua@gmail.com>
2020-07-28 09:37:39 +0200

    ajout de la route get/:id

--- module-3/api-server/routes/api.products.js
+++ module-3/api-server/routes/api.products.js
@@ -1,14 +1,24 @@
 const router = new require("express").Router();
 const ProductModel = require("./../models/Product");
 
+// /api/products/
 router.get("/", async (req, res, next) => {
   try {
-    const products = await ProductModel.find().limit(100);
+    const products = await ProductModel.find().limit(10);
     res.json(products);
   } catch (err) {
     next(err);
   }
 });
 
+// /api/products/un-id-mongo
+router.get("/:id", async (req, res, next) => {
+  try {
+    const product = await ProductModel.findById(req.params.id);
+    res.json(product);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
