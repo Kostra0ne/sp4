0edbb92ebbb6d7f1d871e4666a730f2712735f89
KostraOne <guillaume.amangoua@gmail.com>
2020-07-28 09:40:19 +0200

    ajout d'une vue basique

--- module-3/etsy-clone-react/src/components/page/Contact.jsx
+++ module-3/etsy-clone-react/src/components/page/Contact.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./../../styles/mod.form.css";
 
 export default function About() {
   return (
-    <>
+    <form className="form">
       <h1 className="title">Contact</h1>
-    </>
+    </form>
   );
 }
