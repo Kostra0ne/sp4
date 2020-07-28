3223e4e1f688eab915355bf722a7ef99a3716942
KostraOne <guillaume.amangoua@gmail.com>
2020-07-28 09:39:39 +0200

    ajout d'une version classe du handler d'api

--- module-3/etsy-clone-react/src/api/handler.js
+++ module-3/etsy-clone-react/src/api/handler.js
@@ -1,22 +1,17 @@
 import axios from "axios";
 
-export function APIhandler(APIPrefix) {
-  return (() => {
-    const instance = axios.create({
+export class APIHandler {
+  constructor(APIPrefix) {
+    this.instance = axios.create({
       baseURL: process.env.REACT_APP_BACKEND_URL + APIPrefix || "",
     });
+  }
 
-    const getAll = (path) => instance.get("/");
+  getAll() {
+    return this.instance.get("/"); // retourne une Promesse
+  }
 
-    const getById = (id) => instance.get("/" + id);
-
-    const getOne = (path, query) => instance.get("/" + path, { query });
-
-    return {
-      instance,
-      getAll,
-      getById,
-      getOne,
-    };
-  })();
+  getById(id) {
+    return this.instance.get("/" + id)
+  }
 }
