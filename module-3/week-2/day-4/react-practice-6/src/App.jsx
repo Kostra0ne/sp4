f9fb72977ffa94c0be81a6b288872ced4b791227
KostraOne <guillaume.amangoua@gmail.com>
2020-07-30 12:46:04 +0200

    ajout react practice 6

---
+++ module-3/week-2/day-4/react-practice-6/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/App.css';
+
+import ProductsWidget from "./product/ProductsWidgetSoluce"
+
+function App() {
+  return (
+    <div className="App">
+        <ProductsWidget />
+    </div>
+  );
+}
+
+export default App;
---
+++ module-3/week-2/day-4/react-practice-6/src/product/ProductsWidget.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import data from "./../data/products.json";
+import ProductsCurrencyFilter from "./ProductsCurrencyFilter";
+import ProductDetails from "./ProductDetails";
+import "./../styles/ProductsWidget.css";
+
+
+// attention, on va supprimer les doublons des différentes monnaies, dans ProductsCurrencyFilter
+ 
+export default class ProductsWidget extends Component {
+    render() {
+        return (
+            <div>
+                todo
+            </div>
+        )
+    }
+}
