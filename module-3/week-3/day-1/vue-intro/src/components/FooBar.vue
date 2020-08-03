17f7a89b90d7fb120359c3d6f63b77e243227e06
KostraOne <guillaume.amangoua@gmail.com>
2020-08-03 15:47:19 +0200

    ajout mod3/week3

---
+++ module-3/week-3/day-1/vue-intro/src/components/FooBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <p class="foo-bar">Foo Bar !!!</p>
+</template>
+
+<style lang="scss" scoped>
+$color1: dodgerblue;
+.foo-bar {
+  border: 1px solid $color1;
+  border-radius: 0.3rem;
+  max-width: 120px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+// pour utiliser sass :
+// npm i sass sass-loader
+</style>
+
